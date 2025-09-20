fea1e22736b0751ad5ac3cc7f6e1ef32e1937833
Jemmy 范 <jemmyfan@JemmydeMacBook-Pro.local>
2025-09-21 00:57:46 +0800

    add part of XORList function

--- HW2/113511013_hw2.cpp
+++ HW2/113511013_hw2.cpp
@@ -1,16 +1,16 @@
 #include "113511013_hw2.h"
-#include <list>
 #include <struct>
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <cstdint>
 using namespace std;
 
 //singlelist
-SList::SList(){}
-Slist::SList(Node* h): head = h{}
-SList::~SList(){}
-ofstream& SList::list_walk(ofstream& fout, string& filename){
+SingleList::SingleList(): head(nullptr){}
+SingleList::SingleList(Node* h): head = h{}
+SingleList::~SingleList(): head(nullptr){}
+ofstream& SingleList::list_walk(ofstream& fout, string& filename){
   fout.open(filename);
 
   if (!fout.is_open()){
@@ -28,12 +28,12 @@ ofstream& SList::list_walk(ofstream& fout, string& filename){
 
   return fout;
 }
-void SList::list_insert(int k){
+void SingleList::list_insert(int k){
   head->next = head;
   head->data = k;
   return;
 }
-bool SList::list_search(int k){
+bool SingleList::list_search(int k){
   Node* curr = head;
 
   while (curr != nullptr){
@@ -44,8 +44,13 @@ bool SList::list_search(int k){
 
   if (curr == nullptr) return false;
 }
-void SList::ist_delete(Node* n){
+void SingleList::ist_delete(Node* n){
   Node* curr = head;
+
+  if (curr == n){
+    curr = curr->next;
+    return;
+  }
 
   while(curr != nullptr){
     if (curr->next == n)
@@ -55,7 +60,7 @@ void SList::ist_delete(Node* n){
 
   return;
 }
-int SList::list_ins_del(int k){
+int SingleList::list_ins_del(int k){
   Node* curr = head;
   Node* curr_nxt = curr->next;
   SList S(head);
@@ -74,7 +79,7 @@ int SList::list_ins_del(int k){
     return 0;
   }
 }
-void SList::list_reverse(){
+void SingleList::list_reverse(){
   Node* curr = head;
   Node* prev = nullptr;
   Node* nxt = curr->next;
@@ -87,6 +92,70 @@ void SList::list_reverse(){
   }
 
   head = prev;
-  
+
   return;
 }
+
+//XORList
+Nodex* XOR(Nodex* a, Nodex* b){
+  return (Nodex*)((uintptr_t)(a) ^ (uintptr_t)(b));
+}
+
+XORList::XORList(): head(nullptr){}
+XORList::XORList(Nodex* h): head(h){}
+XORList::~XORList(): head(nullptr){}
+ofstream& XORList::list_walk(ofstream& fout, string& filename){
+  fout.open(filename);
+  if (!fout.is_open()){
+    cerr << "Who is " << filename << "??" << endl;
+    return fout;
+  }
+
+  Nodex* prev = nullptr;
+  Nodex* curr = head;
+  while (curr != nullptr){
+    if (XOR(prev, curr->npx) != nullptr)
+      fout << curr->data << ", ";
+    else fout << curr->data << endl;
+    prev = curr;
+    curr = XOR(prev, curr->npx);
+  }
+  return fout;
+}
+void XORList::list_insert(int k){
+  Nodex* newnode;
+  newnode->data = k;
+  newnode->npx = XOR(nullptr, head);
+  
+  if (head != nullptr){
+    Nodex* next = XOR(nullptr, head->npx);
+    head->npx = XOR(newnode, next);
+  }
+  head = newnode;
+  return;
+}
+bool XORList::list_search(int k){
+  Nodex* curr = head;
+  Nodex* prev = nullptr;
+
+  while(curr != nullptr){
+    if (curr->data == k)
+      return true;
+    Nodex* next = XOR(prev, curr->npx);
+    prev = curr;
+    curr = next;
+  }
+
+  if (curr == nullptr)
+    return false;
+}
+void XORList::list_delete(Nodex* n){
+  Nodex* curr = head;
+  while (curr != nullptr){
+    if (curr == n){
+      
+    }
+  }
+}
+int XORList::list_ins_del(int k);
+void XORList::list_reverse();
--- HW2/113511013_hw2.h
+++ HW2/113511013_hw2.h
@@ -2,9 +2,16 @@
 #include <struct>
 using namespace std;
 
+//node for singlelist
 struct Node{
   int data;
   Node* next;
+}
+
+//node for xorlist
+struct Nodex{
+  int data;
+  Nodex* npx;
 }
 
 class SingleList(){
@@ -20,7 +27,19 @@ public:
   void list_delete(Node* n);
   int list_ins_del(int k);
   void list_reverse();
+}
 
-  class XList(){
+  class XORList(){
   private:
+    Nodex* head;
+  public:
+    XORList();
+    XORList(Nodex* h);
+    ~XORList();
+    ofstream& list_walk(ofstream& fout, string& filename);
+    void list_insert(int k);//insert k to the head of slist
+    bool list_search(int k);
+    void list_delete(Nodex* n);
+    int list_ins_del(int k);
+    void list_reverse();
   }
